002d7d23faa8565052843b5762ac06a42b5047ab
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2025-11-28 12:38:12 +0100

    feat(space-age) : mise d'une const pour les valeurs et suppresion du try/catche qui crée des problème
    [11min][DONE]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,40 +3,35 @@
 // convenience to get you started writing code faster.
 //
 export const age = (planet, age) => {
+  const ORBITAL_PERIODS = { mercury: 0.2408467, venus: 0.61519726, earth: 1.0, mars: 1.8808158, jupiter: 11.862615, saturn: 29.447498, uranus: 84.016846, neptune: 164.79132, };
   let years
-  try
-  {
     if(planet === "mercury"){
-      years = Number((age/(31557600*0.2408467)).toFixed(2))
+      years = Number((age/(31557600*ORBITAL_PERIODS.mercury)).toFixed(2))
 
     }
       else if(planet ==="venus"){
-      years = Number((age/(31557600*0.61519726)).toFixed(2))
+      years = Number((age/(31557600*ORBITAL_PERIODS.venus)).toFixed(2))
     }
       else if(planet === "earth"){
       years = Number((age/31557600).toFixed(2))
     }
       else if(planet ==="mars"){
-      years = Number((age/(31557600*1.8808158)).toFixed(2))
+      years = Number((age/(31557600*ORBITAL_PERIODS.mars)).toFixed(2))
     }
       else if(planet ==="jupiter"){
-      years = Number((age/(31557600*11.862615)).toFixed(2))
+      years = Number((age/(31557600*ORBITAL_PERIODS.jupiter)).toFixed(2))
     }
       else if(planet ==="saturn"){
-      years = Number((age/(31557600*29.447498)).toFixed(2))
+      years = Number((age/(31557600*ORBITAL_PERIODS.saturn)).toFixed(2))
     }
       else if(planet ==="uranus"){
-      years = Number((age/(31557600*84.016846)).toFixed(2))
+      years = Number((age/(31557600*ORBITAL_PERIODS.uranus)).toFixed(2))
     }
       else if(planet ==="neptune"){
-      years = Number((age/(31557600*164.79132)).toFixed(2))
+      years = Number((age/(31557600*ORBITAL_PERIODS.neptune)).toFixed(2))
     }
     else{
       throw new Error("not a planet");
     }
     return years
   }
-  catch (e) {
-    console.error(`${e.name}: ${e.message}`);
-  }
-};
