d8953c499c2c009cf8bd3b064c6f7e1aee0ecd54
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2025-12-05 13:31:51 +0100

    feat(space-age) : supression des if pour en avoir un seul
    [28min][DONE]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -5,30 +5,9 @@
 export const age = (planet, age) => {
   const ORBITAL_PERIODS = { mercury: 0.2408467, venus: 0.61519726, earth: 1.0, mars: 1.8808158, jupiter: 11.862615, saturn: 29.447498, uranus: 84.016846, neptune: 164.79132, };
   let years
-    if(planet === "mercury"){
-      years = Number((age/(31557600*ORBITAL_PERIODS.mercury)).toFixed(2))
-
-    }
-      else if(planet ==="venus"){
-      years = Number((age/(31557600*ORBITAL_PERIODS.venus)).toFixed(2))
-    }
-      else if(planet === "earth"){
-      years = Number((age/31557600).toFixed(2))
-    }
-      else if(planet ==="mars"){
-      years = Number((age/(31557600*ORBITAL_PERIODS.mars)).toFixed(2))
-    }
-      else if(planet ==="jupiter"){
-      years = Number((age/(31557600*ORBITAL_PERIODS.jupiter)).toFixed(2))
-    }
-      else if(planet ==="saturn"){
-      years = Number((age/(31557600*ORBITAL_PERIODS.saturn)).toFixed(2))
-    }
-      else if(planet ==="uranus"){
-      years = Number((age/(31557600*ORBITAL_PERIODS.uranus)).toFixed(2))
-    }
-      else if(planet ==="neptune"){
-      years = Number((age/(31557600*ORBITAL_PERIODS.neptune)).toFixed(2))
+  const second = 31557600
+    if(planet in ORBITAL_PERIODS){
+      years = Number((age/(second*ORBITAL_PERIODS[planet])).toFixed(2))
     }
     else{
       throw new Error("not a planet");
