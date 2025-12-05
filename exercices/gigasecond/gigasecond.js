3ade140e7e0d177c18849a8780c80dc6756539d9
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2025-12-05 16:02:22 +0100

    feat(exercice) gigasecond
    [28min][DONE]

--- exercices/gigasecond/gigasecond.js
+++ exercices/gigasecond/gigasecond.js
@@ -2,7 +2,7 @@
 // This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
-export const gigasecond = () => {
-  throw new Error('Remove this line and implement the function');
+// 31ans 8mois 8jour 1h 46min 40sec 
+export const gigasecond = (date) => {
+  return new Date(date.getTime() + 1e12)
 };
