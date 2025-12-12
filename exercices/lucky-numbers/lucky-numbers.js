e2ad5385a9ac4ed262f38c1e18d05e3c8273534c
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2025-12-12 15:33:35 +0100

    feat(exercice) : Lucky-numbers
    [95min][WIP]

--- exercices/lucky-numbers/lucky-numbers.js
+++ exercices/lucky-numbers/lucky-numbers.js
@@ -8,7 +8,17 @@
  * @returns {number} sum of the two arrays
  */
 export function twoSum(array1, array2) {
-  throw new Error('Remove this line and implement the function');
+  let num1 = "";
+  let num2 = "";
+  for(let i=0; i>array1.length-1; i++){
+    num1+array1[i]
+  }
+  
+  for(let i=0; array2.length-1;i++){
+    num2+array2[i]
+  }
+  let sumNum = Number(num1)+Number(num2)
+  return sumNum
 }
 
 /**
@@ -18,7 +28,19 @@ export function twoSum(array1, array2) {
  * @returns {boolean} whether the number is a palindrome or not
  */
 export function luckyNumber(value) {
-  throw new Error('Remove this line and implement the function');
+  if (value < 0) return false; // Les nombres négatifs ne sont pas des palindromes
+
+    let original = value;
+    let nombreInverse = 0;
+
+    while (value > 0)
+    {
+        let digit = value % 10; // Dernier chiffre
+        nombreInverse = nombreInverse * 10 + digit; // Ajoute le chiffre à l'inverse
+        value /= 10; // Retire le dernier chiffre
+    }
+
+    return original == nombreInverse;
 }
 
 /**
@@ -29,5 +51,14 @@ export function luckyNumber(value) {
  * @returns {string} error message
  */
 export function errorMessage(input) {
-  throw new Error('Remove this line and implement the function');
+  if (input = null){
+    return input
+  }
+  else if (Number(input) < 0){
+    return 'Must be a number besides 0'
+  }
+  else{
+    return " "
+  }
+
 }
