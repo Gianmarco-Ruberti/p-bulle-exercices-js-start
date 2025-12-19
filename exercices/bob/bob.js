867f6bbd1ba4df7b46a2c64337249f3a434712bd
Gianmarco Ruberti <py13koy@eduvaud.ch>
2025-12-19 14:04:46 +0100

    feat(exercice) bob
    [21min][DONE]

--- exercices/bob/bob.js
+++ exercices/bob/bob.js
@@ -4,5 +4,29 @@
 //
 
 export const hey = (message) => {
-  throw new Error('Remove this line and implement the function');
+  // supprime les espace
+  const trimmed = message.trim();
+  // verifie si se termine par ?
+  const isQuestion = trimmed.endsWith('?');
+  // verifie si tout est en majuscules avec trimmed et contient au moins une lettre majuscule dans le texte avec le .test
+  const isYelling = trimmed.toUpperCase() === trimmed && /[A-Z]/.test(trimmed);
+
+  if (trimmed === '') {
+    return 'Fine. Be that way!';
+  }
+
+  if (isQuestion && isYelling) {
+    return "Calm down, I know what I'm doing!";
+  }
+
+  if (isYelling) {
+    return 'Whoa, chill out!';
+  }
+
+  if (isQuestion) {
+    return 'Sure.';
+  }
+
+  return 'Whatever.';
 };
+
