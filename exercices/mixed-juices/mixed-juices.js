4bdb3908579f1a2e0773ab8aa5a75e559f510a2d
Gianmarco Ruberti <py13koy@eduvaud.ch>
2025-12-19 14:45:20 +0100

    feat(exercice) mixed-juices
    [35min][DONE]

--- exercices/mixed-juices/mixed-juices.js
+++ exercices/mixed-juices/mixed-juices.js
@@ -11,7 +11,19 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Remove this line and implement the function');
+  switch (name) {
+    case 'Pure Strawberry Joy':
+      return 0.5;
+    case 'Energizer':
+    case 'Green Garden':
+      return 1.5;
+    case 'Tropical Island':
+      return 3;
+    case 'All or Nothing':
+      return 5;
+    default:
+      return 2.5;
+    }
 }
 
 /**
@@ -23,7 +35,28 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Remove this line and implement the function');
+  let wedgesCut = 0;
+  let limesCut = 0;
+
+  for (const lime of limes) {
+    if (wedgesCut >= wedgesNeeded) {break;}
+
+    switch (lime) {
+      case 'small':
+        wedgesCut += 6;
+        break;
+      case 'medium':
+        wedgesCut += 8;
+        break;
+      case 'large':
+        wedgesCut += 10;
+        break;
+    }
+
+    limesCut++;
+  }
+
+  return limesCut;
 }
 
 /**
@@ -34,5 +67,15 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Remove this line and implement the function');
+ let i = 0;
+  
+  while (timeLeft > 0 && i < orders.length) {
+    const juiceTime = timeToMixJuice(orders[i]);
+    timeLeft -= juiceTime;
+    i++;
+  }
+
+  return orders.slice(i);
 }
+
+
